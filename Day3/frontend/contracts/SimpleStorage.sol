32420eb491fcfafca48c16468bd0e87f1f169b8b
Ashish Maharjan <amaharjan1032@gmail.com>
2021-06-17 10:36:44 +0545

    Day3 done

--- Day3/frontend/contracts/SimpleStorage.sol
+++ Day3/frontend/contracts/SimpleStorage.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.5.0;
 
 contract SimpleStorage {
-  uint storedData;
+    uint256 storedData;
 
-  function set(uint x) public {
-    storedData = x;
-  }
+    function setData(uint256 x) public {
+        storedData = x;
+    }
 
-  function get() public view returns (uint) {
-    return storedData;
-  }
+    function getData() public view returns (uint256) {
+        return storedData;
+    }
 }
--- Day3/frontend/public/bundle.js
+++ Day3/frontend/public/bundle.js
@@ -1,7 +1,63 @@
-var simpleStorageABI = [];
-var simpleStorageAddress = '';
+var simpleStorageABI = [
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "x",
+                "type": "uint256"
+            }
+        ],
+        "name": "setData",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "getData",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    }
+];
+var simpleStorageAddress = '0x621732ec749650188bC48c862587d763C970EB4c';
 var web3 = new Web3('http://localhost:9545');
 var simpleStorage = new web3.eth.Contract(simpleStorageABI, simpleStorageAddress);
 
 document.addEventListener('DOMContentLoaded', () => {
+    const $setValue = document.getElementById('setData');
+    const $data = document.getElementById('data');
+    let accounts = [];
+
+    web3.eth.getAccounts()
+        .then(_accounts => {
+            accounts = _accounts;
+        });
+
+    const getValue = () => {
+        simpleStorage.methods.getData().call().then(result => {
+            $data.innerHTML = result;
+        });
+    };
+    getValue();
+
+    $setValue.addEventListener('submit', e => {
+        e.preventDefault();
+        const data = e.target.elements[0].value;
+        simpleStorage.methods
+            .setData(data)
+            .send({ from: accounts[0] })
+            .then(getValue);
+    });
 });
+
